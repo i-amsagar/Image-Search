d92b57f392290401491a191a49eddd2db8f7408e
Sagar <sagar00001.co@gmail.com>
2023-08-05 19:26:12 +0530

    first commit

---
+++ src/context.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AppContext = createContext();
+
+const getInitialDarkMode = () => {
+  const preferDarkMode = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+  const storesDarkMode = localStorage.getItem("darkTheme") === "true";
+  return storesDarkMode || preferDarkMode;
+};
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+  const [searchTerm, setSearchTerm] = useState("Tiger");
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme);
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
+
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
